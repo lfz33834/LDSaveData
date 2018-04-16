2fed9d7aa2fd75a4153a20de964e1ad74a294af7
lifuzhou <fuzhou_li@qingsongchou.com>
2018-04-16 16:28:00 +0800

    fix

--- LDSaveData/ViewController.m
+++ LDSaveData/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <EGOCache.h>
 
 @interface ViewController ()
 
@@ -16,7 +17,53 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    EGOCache *egocache = [[EGOCache globalCache]initWithCacheDirectory:[self createCacheDirection]];
+    //清除缓存
+//    [egocache clearCache];
+    //设置缓存时间 默认时间一天  一天的时间表示：24*60*60
+    egocache.defaultTimeoutInterval = 60;
+    //缓存文字
+    NSString *cacheString = @"111111111111";
+    [egocache setString:cacheString forKey:@"string"];
+    //是否有这个缓存
+    BOOL ishaveCacheFile = [egocache hasCacheForKey:@"string"];
+    if (ishaveCacheFile) {
+        //读取文字
+        NSString *currentCacheString = [egocache stringForKey:@"string"];
+        NSLog(@"缓存的文字：%@",currentCacheString);
+    }
+    
+}
+
+/**
+ *  创建缓存目录
+ *
+ *  @return 缓存目录
+ */
+-(NSString *)createCacheDirection
+{
+    //沙盒目录
+    NSLog(@"-----%@",NSHomeDirectory());
+    //Document 文件夹目录
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *pathDocuments = [paths objectAtIndex:0];
+    //创建缓存目录
+    NSString *createPath = [NSString stringWithFormat:@"%@/MessageCache", pathDocuments];
+    // 判断文件夹是否存在，如果不存在，则创建
+    if (![[NSFileManager defaultManager] fileExistsAtPath:createPath]) {
+        NSFileManager *fileManager = [[NSFileManager alloc] init];
+        BOOL result = [fileManager createDirectoryAtPath:createPath withIntermediateDirectories:YES attributes:nil error:nil];
+        if (result) {
+            return createPath;
+        }else
+        {
+            return nil;
+        }
+    } else {
+        NSLog(@"FileDir is exists.");
+        return createPath;
+    }
 }
 
 
---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '8.0'
+
+target 'LDSaveData' do
+  # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
+  # use_frameworks!
+pod 'EGOCache', '~> 2.1.3'
+  # Pods for UseProtobufDemo
+
+end
